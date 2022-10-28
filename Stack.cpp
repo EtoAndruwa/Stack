321a1137f630922cc08f9a15ad0f62780dea4603
EtoAndruwa <timonina1909@gmail.com>
2022-10-29 01:45:26 +0300

    Сделал typedef stack_type, добавил функцию StackDiv

--- Stack.cpp
+++ Stack.cpp
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef double stack_type;
+
 typedef struct
     {
-        int * data;
+        stack_type * data;
         int size = 1;
 
     }Stack;
 
 void StackInit(Stack * st);
-void StackPush(Stack * st, int push_value);
+void StackPush(Stack * st, stack_type push_value);
 //int StackCheck(Stack * st);
 void StackDamp(Stack * st);
-int StackPop(Stack * st);
+stack_type StackPop(Stack * st);
 void StackMul(Stack * st);
 void StackSub(Stack * st);
+void StackDiv(Stack * st);
 
 enum Error_list
     {
@@ -28,34 +31,34 @@ int error_code = 0;
 int main()
     {
         Stack a;
-        int push_value = 0;
-        int poped_value = 0;
+        stack_type push_value = 0;
+        stack_type poped_value = 0;
 
         StackInit(&a);
 
-        scanf("%d", &push_value);
+        scanf("%lf", &push_value);
         StackPush(&a, push_value);
         StackDamp(&a);
 
-        scanf("%d", &push_value);
+        scanf("%lf", &push_value);
         StackPush(&a, push_value);
         StackDamp(&a);
 
-        scanf("%d", &push_value);
+        scanf("%lf", &push_value);
         StackPush(&a, push_value);
         StackDamp(&a);
 
-        scanf("%d", &push_value);
+        scanf("%lf", &push_value);
         StackPush(&a, push_value);
         StackDamp(&a);
 
-        scanf("%d", &push_value);
+        scanf("%lf", &push_value);
         StackPush(&a, push_value);
         StackDamp(&a);
 
         StackDamp(&a);
         poped_value = StackPop(&a);
-        printf("Poped value = %d\n", poped_value);
+        printf("Poped value = %.3lf\n", poped_value);
         StackDamp(&a);
 
         StackMul(&a);
@@ -74,20 +77,20 @@ void StackInit(Stack * st)
     {
         stack_size = 5;
         st->size = 0;
-        st->data = (int *)calloc(stack_size, sizeof(int));
+        st->data = (stack_type *)calloc(stack_size, sizeof(stack_type));
 //        StackCheck(st);
     }
 
-void StackPush(Stack * st, int push_value)
+void StackPush(Stack * st, stack_type push_value)
     {
 //        StackCheck(st);
         st->data[st->size++] = push_value;
 //        StackCheck(st);
     }
 
-int StackPop(Stack * st)
+stack_type StackPop(Stack * st)
     {
-        int temp = st->data[st->size - 1];
+        stack_type temp = st->data[st->size - 1];
         st->data[st->size - 1] = 0;
         st->size--;
         return temp;
@@ -126,11 +129,11 @@ void StackDamp(Stack * st)
             {
                 if(i == stack_size-1)
                     {
-                        printf("data[%d] = %d\n\n", i, st->data[i]);
+                        printf("data[%d] = %.3lf\n\n", i, st->data[i]);
                     }
                 else
                     {
-                        printf("data[%d] = %d\n", i, st->data[i]);
+                        printf("data[%d] = %.3lf\n", i, st->data[i]);
                     }
             }
     }
@@ -145,6 +148,13 @@ void StackMul(Stack * st)
 void StackSub(Stack * st)
     {
         st->data[st->size - 2] = st->data[st->size - 2] - st->data[st->size - 1];
+        st->data[st->size - 1] = 0;
+        st->size--;
+    }
+
+void StackDiv(Stack * st)
+    {
+        st->data[st->size - 2] = (st->data[st->size - 2]) / (st->data[st->size - 1]);
         st->data[st->size - 1] = 0;
         st->size--;
     }
